a78460b15443e388145177c04a91a4cf6fd3a53e
Terano <102757870+Danil0666@users.noreply.github.com>
2022-05-16 14:17:55 -0300

    Add files via upload

---
+++ ativ/atividade2.js
@@ -0,0 +1,24 @@
+//1
+let c = 1
+while (c <= 100){
+    console.log(c);
+     c = c + 1
+}
+//2
+let d = 50
+while (d <= 100){
+    console.log(d);
+     d = d + 1
+}
+//3
+let G = 10
+while (G >= 0){
+    console.log(G);
+     G = G - 1
+}
+4//
+let b = 0
+while (b <= 20){
+    console.log(b);
+     b = b + 2
+}
